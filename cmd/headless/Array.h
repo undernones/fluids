3b11248aa3bd0da01bd79b4282246c6659fa774d
Stephen Ward <sjward@gmail.com>
2013-08-13 22:01:03 -0700

    Starting a 3d array class.

---
+++ cmd/headless/Array.h
@@ -0,0 +1,82 @@
+// License: See distributed LICENSE file.
+
+#ifndef FLUID_ARRAY_H
+#define FLUID_ARRAY_H
+
+#include <vector>
+#include <stdexcept>
+
+/// A 3-dimensional array of objects of any type.
+template <class T>
+class Array
+{
+public:
+    // Default constructor
+    //
+    explicit Array()
+        : mData()
+        , mWidth(0)
+        , mHeight(0)
+        , mDepth(0)
+    {}
+
+    // Typical constructor
+    //
+    explicit Array(unsigned width, unsigned height, unsigned depth)
+        : mData()
+        , mWidth(width)
+        , mHeight(height)
+        , mDepth(depth)
+    {
+        mData.resize(width * height * depth);
+    }
+
+    // Copy constructor
+    explicit Array(const Array& other)
+        : mData(other.mData)
+        , mWidth(other.mWidth)
+        , mHeight(other.mHeight)
+        , mDepth(other.mDepth)
+    {}
+
+    // Destructor
+    //
+    ~Array()
+    {}
+
+    // Assignment operator
+    //
+    Array& operator =(const Array& rhs)
+    {
+        if (this != &rhs) {
+            mData = rhs.mData;
+            mWidth = rhs.mWidth;
+            mHeight = rhs.mHeight;
+            mDepth = rhs.mDepth;
+        }
+        return *this;
+    }
+
+    unsigned size() const
+    {
+        return mData.size();
+    }
+
+    const T& operator ()(unsigned i, unsigned j, unsigned k) const
+    {
+        if (i >= mWidth) throw std::out_of_range("i");
+        if (j >= mHeight) throw std::out_of_range("j");
+        if (k >= mDepth) throw std::out_of_range("k");
+
+        return mData[(i*mHeight*mDepth) + (j*mDepth) + k];
+    }
+
+private:
+    std::vector<T> mData;
+    unsigned mWidth;
+    unsigned mHeight;
+    unsigned mDepth;
+};
+
+#endif // FLUID_ARRAY_H
+
--- cmd/headless/main.cpp
+++ cmd/headless/main.cpp
@@ -1,8 +1,40 @@
+// License: See distributed LICENSE file.
+
 #include <iostream>
+#include "Array.h"
+
+namespace
+{
+
+const int SUCCESS = 0;
+const int ERROR = 1;
+
+}
+
+class Something
+{
+};
 
 int
 main(int argc, char* argv[])
 {
-    std::cout << "hello, world" << std::endl;
-    return 0;
+    try {
+        Array<double> arrd1;
+        Array<double> arrd2(4, 5, 2);
+        Array<int> arri;
+        Array<Something> arrs(2, 3, 4);
+
+        std::cout << arrd1.size() << std::endl
+                  << arrd2.size() << std::endl
+                  << arri.size() << std::endl
+                  << arrs.size() << std::endl
+                  ;
+
+        std::cout << arrd1(0, 0, 0);
+    } catch (std::exception& e) {
+        std::cout << "Exception: " << e.what() << std::endl;
+        return ERROR;
+    }
+
+    return SUCCESS;
 }
